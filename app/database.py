0a7c056d38ea20546fadf254870afc60ccefdb1c
Silky <silky@localhost.com>
2026-04-10 12:55:50 +0200

    feat: Implemented DB session dependency injection

--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import Session, sessionmaker, declarative_base
 
 DATABASE_URL = "mysql+pymysql://root:root@localhost/fastapi_users"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db:Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
--- app/routes.py
+++ app/routes.py
@@ -1,54 +1,46 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from .models import User
 from .schemas import UserCreate, UserRead
-from .database import SessionLocal
+from .database import get_db
 
 router = APIRouter()
 
 # Create user
 @router.post("/users/", response_model=UserRead)
-def create_user(user: UserCreate):
-    db: Session = SessionLocal()
+def create_user(user: UserCreate, db:Session = Depends(get_db)):
     db_user = User(name=user.name, email=user.email)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    db.close()
+   
     return db_user
 
 # Get all users
 @router.get("/users/", response_model=list[UserRead])
-def get_users():
-    db: Session = SessionLocal()
+def get_users(db:Session = Depends(get_db)):
     users = db.query(User).all()
-    db.close()
     return users
 
 # Update user
 @router.put("/users/{user_id}", response_model=UserRead)
-def update_user(user_id: int, updated_user: UserCreate):
-    db: Session = SessionLocal()
+def update_user(user_id: int, updated_user: UserCreate,db:Session = Depends(get_db)):
     db_user = db.query(User).filter(User.id == user_id).first()
     if not db_user:
-        db.close()
         raise HTTPException(status_code=404, detail="User not found")
     db_user.name = updated_user.name
     db_user.email = updated_user.email
     db.commit()
     db.refresh(db_user)
-    db.close()
     return db_user
 
 # Delete user
 @router.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    db: Session = SessionLocal()
+def delete_user(user_id: int,db:Session = Depends(get_db)):
     db_user = db.query(User).filter(User.id == user_id).first()
     if not db_user:
         db.close()
         raise HTTPException(status_code=404, detail="User not found")
     db.delete(db_user)
     db.commit()
-    db.close()
     return {"message": "User deleted"}
